60e1200f43200e6e9ee2202a4d4cdb052d604efb
MelladoDev <Mellado.Dev@gmail.com>
2024-11-01 01:38:47 -0300

    emailx

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,55 +1,4 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/userModel');
-const bcrypt = require('bcryptjs');
-const pool = require('./../db/db');
 
-
-
-exports.register = async (req, res) => {
-  const { nombre, email, password} = req.body;
-  try {
-    const userExists = await pool.query('SELECT * FROM usuarios WHERE email = $1', [email]);
-    if (userExists.rowCount > 0) {
-      return res.status(400).json({ error: 'El usuario ya existe' });
-    }
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = await pool.query(
-      'INSERT INTO usuarios (nombre, email, password) VALUES ($1, $2, $3) RETURNING *',
-      [ nombre, email, hashedPassword ]
-    );
-
-    res.status(201).json(newUser.rows[0]);
-  } catch (err) {
-    console.error('Error al registrar usuario:', err);
-    res.status(500).json({ error: 'Error del servidor' });
-  }
-};
-
-exports.login = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const user = await User.findByEmail(email);
-    if (!user) return res.status(404).json({ error: 'Usuario no encontrado' });
-
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) return res.status(401).json({ error: 'Credenciales incorrectas' });
-
-    const token = jwt.sign({ email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.json({ token });
-  } catch (err) {
-    res.status(500).json({ error: 'Error al iniciar sesión' });
-  }
-};
-
-exports.getUser = async (req, res) => {
-  try {
-    const user = await User.findByEmail(req.user.email);
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ error: 'Error al obtener usuario' });
-  }
-};
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
@@ -72,13 +21,13 @@ exports.register = async (req, res) => {
     );
 
     
-    const content = {
-      to: email,
-      subject: 'Registro exitoso',
-      html: `<h1>Bienvenido ${nombre} a Industric</h1>`,
-    };
+    // const content = {
+    //   to: email,
+    //   subject: 'Registro exitoso',
+    //   html: `<h1>Bienvenido ${nombre} a Industric</h1>`,
+    // };
 
-    mailSender(content);
+    // mailSender(content);
 
     res.status(201).json(newUser.rows[0]);
 
--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,30 +1,30 @@
 // using Twilio SendGrid's v3 Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
 
-dotenv.config({ path: 'server.env' });
-const sgMail = require('@sendgrid/mail')
-const dotenv = require('dotenv');
-sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-const fromEmail = process.env.FROM_EMAIL;
-const sendGridApiKey = process.env.SENDGRID_API_KEY;
-sgMail.setApiKey(sendGridApiKey);
+// dotenv.config({ path: 'server.env' });
+// const sgMail = require('@sendgrid/mail')
+// const dotenv = require('dotenv');
+// sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+// const fromEmail = process.env.FROM_EMAIL;
+// const sendGridApiKey = process.env.SENDGRID_API_KEY;
+// sgMail.setApiKey(sendGridApiKey);
 
-function mailSender(msg) {
-    const msg = {
-        to: msg.toMails,
-        from: fromEmail,
-        subject: msg.subject,
-        html: msg.content,
-    }
-    sgMail
-        .send(msg)
-        .then(() => {
-            console.log('Email sent')
-        })
-        .catch((error) => {
-            console.error(error)
-        })
+// function mailSender(msg) {
+//     const msg = {
+//         to: msg.toMails,
+//         from: fromEmail,
+//         subject: msg.subject,
+//         html: msg.content,
+//     }
+//     sgMail
+//         .send(msg)
+//         .then(() => {
+//             console.log('Email sent')
+//         })
+//         .catch((error) => {
+//             console.error(error)
+//         })
 
-}
+// }
 
-module.exports = mailSender;
+// module.exports = mailSender;
